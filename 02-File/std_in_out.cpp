3ec3fe5744586e4d9a48ead1e83613d34db460f1
ancion <ancion@github.com>
2024-12-01 11:14:59 +0800

    in/out format

--- 02-File/std_in_out.cpp
+++ 02-File/std_in_out.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <string>
 
+// C 中使用 printf() 进行格式化输出
+//
+// %c  :  char
+// %u  :  unsign number
+// %d  :  int
+// %ld :  long 
+// %hd :  short
+// %f  :  float, 默认是7 位小数, 可以使用 `%.nf`  使用 n 确定保留的小数位数
+// %lf :  double
+// %s  :  string
+// %x  :  hex (十六进制输出)
+// %o  :  Oct (八进制输出)
+
+
 // iostream 文件中定义的 std 包中规定了控制台的标准输入输出
 //
 // <<  表示输出的方向，可以像管道一样，多次拼接
