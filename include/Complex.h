c09c572bfbba5c02e678a086d333f2d3a2f01226
ancion <ancion@github.com>
2025-03-02 22:43:42 +0800

    Complex class

---
+++ include/Complex.h
@@ -0,0 +1,20 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <ostream>
+class Complex {
+public:
+  Complex(double real, double imag);
+  Complex operator+(const Complex& other) const;
+  Complex operator-(const Complex& other) const;
+  Complex operator*(const Complex& other) const;
+  Complex operator/(const Complex& other) const;
+  void print() const;
+  friend std::ostream& operator << (std::ostream& os, const Complex& c);
+private:
+  double real;
+  double imag;
+};
+
+
+#endif // !COMPLEX_H;
