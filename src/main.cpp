432198e5ebf0ad103d7d81bcbc8dc1fda540f833
ancion <ancion@github.com>
2025-03-13 23:19:20 +0800

    src update

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
-#include "../include/Shape.h"
 #include "../include/cmod.h"
 #include "../include/cppmod.h"
-#include "../include/Complex.h"
 
 
 int main (int argc, char *argv[]) {
 
-  // oop 测试
-  // oop_shape_game();
-
   // time 使用规则
   // timeops();
 
-  // 测试复数库的使用
-  Complex c(1, 2);
-  Complex a(2, 2);
-  cout << c + a << endl;
+  // vector_usage();
 
-  vector_usage();
+  // 
+  // str_usage();
+
+  // 
+  // oop_feature();
+
+  // new_delete_memory();
+
+  coroutine();
 
   return 0;
 }
