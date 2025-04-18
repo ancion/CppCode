a22bcd479d8756787bad0fc491deb391d59ecdfc
ancion <ancion@github.com>
2025-04-18 17:07:31 +0800

    add variadic_template

---
+++ 02-Grammer/variadic_template.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T> T addr(T v) { return v; }
+
+// Variable number of input arguments in a template 
+// "typename... Args" is a function parameter pack 
+// we can use recursive calls to unpack all the arguments!
+template <typename T, typename... Args> T addr(T first, Args... args) {
+  // 多个参数的时候会递归调用当前函数展开，最终剩下一个参数时调用上面的 addr
+  // 函数
+  return first + addr(args...);
+}
+
+void func_variadic_template() {
+
+  // We can calculate a sum variable number of integers
+  int int_sum = addr(1, 2, 3, 5, 7);
+  cout << "The integers is " << int_sum << endl;
+
+  // We can calculate a sum with a variable number of folat 
+  float float_sum = addr(1.5, 2.5, 3.5, 5.5, 7.5);
+  cout << "The float is " << float_sum << endl;
+
+  // Declare some string we want to combine
+  string partial1 = "Hello";
+  string partial2 = "World";
+  string partial3 = "!!!";
+
+  // We can concatenate a variable number of strings
+  string concatenate = addr(partial1, partial2, partial3);
+  cout << concatenate << endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ int main (int argc, char *argv[]) {
   // pointer_usage();
 
   // coroutine();
+  coroutine();
 
   return 0;
 }
